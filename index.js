7309fda1163e51869cd90008e52a3307cf9c85c5
sebastianrejas11-stack <sebastianrejas11@gmail.com>
2025-09-18 02:05:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+// index.js
 if (!globalThis.crypto) globalThis.crypto = require("crypto").webcrypto;
 if (!globalThis.Buffer) globalThis.Buffer = require("buffer").Buffer;
 
 const { connectToWhatsApp } = require("./src/bootstrap");
 connectToWhatsApp().catch(e => console.error("Error general:", e?.message));
-
